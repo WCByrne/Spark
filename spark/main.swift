c6ef15f9dc6f3ab64568487872eeaa9aa85d670d
Wes Byrne <wesburn@me.com>
2018-03-30 18:59:21 -0700

    Initial Commit

---
+++ spark/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  spark
+//
+//  Created by Wesley Byrne on 3/30/18.
+//  Copyright © 2018 thenounproject. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
